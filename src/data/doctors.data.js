bbefc8c08469675c17c2de55e397b391883c00a1
DESKTOP-2J5QKLV\Felckie <albaladejofelcon31@gmail.com>
2021-12-21 09:06:27 +0800

    update on doctors

---
+++ src/data/doctors.data.js
@@ -0,0 +1,55 @@
+export const Doctors = [
+  {
+    id: "001",
+    name: "Dr. Leonilo Del Carmen",
+    designation: "SURGERY",
+    description: "",
+    teleSchedule: ["By Appoitment"],
+  },
+  {
+    id: "002",
+    name: "Dr. Dolores Romella Tiples-Ruiz, MD",
+    designation: "INTERNIST | INFECTIOUS DISEASE CONSULTANT",
+    description: "",
+    teleSchedule: [
+      "Tuesday - 2PM",
+      "Tuesday - 4PM",
+      "Thursday - 2PM",
+      "Thursday - 4PM",
+    ],
+  },
+  {
+    id: "003",
+    name: "Dr. Leonard Baraquia, MD",
+    designation: "INTERNAL MEDICINE | PULMONARY MEDICINE | LIFESTYLE MEDICINE",
+    description: "",
+    teleSchedule: ["By Appoitment"],
+  },
+  {
+    id: "004",
+    name: "Dr. Melvin L. Ibanez, MBA, MD",
+    designation:
+      "INTERNAL MEDICINE-CARDIOVASCULAR | DISEASES GERIATRIC AND FAMILY MEDICINE",
+    description: "",
+    teleSchedule: [
+      "Tuesday - 2PM",
+      "Tuesday - 4PM",
+      "Thursday - 2PM",
+      "Thursday - 4PM",
+    ],
+  },
+  {
+    id: "005",
+    name: "",
+    designation: "",
+    description: "",
+    teleSchedule: [],
+  },
+  {
+    id: "006",
+    name: "",
+    designation: "",
+    description: "",
+    teleSchedule: [],
+  },
+];
